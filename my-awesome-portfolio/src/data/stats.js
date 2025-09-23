bd86393ac35b316c40fe9acec454462895221a75
Esshaan Mahajan <Esshaan.Mahajan@gmail.com>
2025-09-22 22:33:10 -0400

    Updates

--- my-awesome-portfolio/src/data/stats.js
+++
@@ -1,7 +0,0 @@
-export const stats = [
-  { label: "Years Coding", value: "6+", icon: "💻" },
-  { label: "Published Papers", value: "2", icon: "📝" },
-  { label: "Projects Led", value: "7+", icon: "🚀" },
-  { label: "Major Awards", value: "🥇" , icon: "🏆"},
-  { label: "GPA (Grad)", value: "3.9", icon: "🎓" },
-];
