9d8b53107a009d39bcb2ab6fae3cd67979881827
Esshaan Mahajan <Esshaan.Mahajan@gmail.com>
2025-09-22 23:19:28 -0400

    Updates

--- my-awesome-portfolio/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer>
-    &copy; {new Date().getFullYear()} Esshaan Mahajan • All Rights Reserved
-  </footer>
-);
-
-export default Footer;
